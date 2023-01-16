04236488afb84ef983f48a33229b7520af7008e9
ForgeIon <Shadow@SHADOW-JH7PDMT5>
2023-01-15 19:46:25 -0600

    spring initial Added

---
+++ src/main/java/net/mcreator/kurostools/procedures/SpringUpdateTickProcedure.java
@@ -0,0 +1,10 @@
+package net.mcreator.kurostools.procedures;
+
+import java.util.Map;
+
+public class SpringUpdateTickProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
